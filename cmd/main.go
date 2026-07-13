ac4f4b0dd22db0f96e2bede9f64c24b290e79b44
luxgru35 <tokilightrb@gmail.com>
2026-07-13 22:48:09 +0400

    feat: добавил конфиг, viper

--- cmd/main.go
+++ cmd/main.go
@@ -7,14 +7,24 @@ import (
 	"github.com/luxgru35/todo-app/pkg/handler"
 	"github.com/luxgru35/todo-app/pkg/repository"
 	"github.com/luxgru35/todo-app/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing config: %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	srv := new(todo.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("failed to run server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
